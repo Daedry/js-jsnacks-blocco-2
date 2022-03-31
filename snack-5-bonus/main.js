13e3032d2878e3bd462508f66f674f3273e79e49
MyUsername <romamargine@gmail.com>
2022-03-31 17:44:54 +0200

    Add Snack 5 bonus

---
+++ snack-5-bonus/main.js
@@ -0,0 +1,23 @@
+/*  
+Snack5 (Bonus)
+Crea due array che hanno un numero di elementi diversi. 
+Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+*/
+
+const stones = ['Azeztulite', 'Moldavite', 'Nummite', 'Apofillite', 'Galena', 'Shungite'];
+const numbers = [23,43,55,66];
+
+let i = 0;
+while (i < stones.length){
+
+    if(numbers.length < stones.length){
+        const randomNum = Math.ceil(Math.random() * 50)
+        numbers.push(randomNum);
+    }
+    
+    i++
+}
+
+console.log(numbers)
+
+
